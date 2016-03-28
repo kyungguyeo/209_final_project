46eff72ca132b0d918805401e8ed06f8b1bc796a
Johnny Yeo <johnnyyeo12@gmail.com>
2016-03-28 15:53:26 -0700

    added footer, added yelp photo functionality

--- scripts/campus_map.js
+++ scripts/campus_map.js
@@ -1,5 +1,14 @@
 var data;    
 var campus_businesses;
+var myStyle = [
+  {
+    featureType: "poi",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
 d3.json("209_FinalProject_Data/food_businesses_by_campus.json", function(obj) {
   campus_businesses = obj;
 });
@@ -13,9 +22,14 @@ function initMap() {
 		var latitude = $.grep(all_data, function(e){ return e.Campus == current_campus})[0].Lat;
 		lt = parseFloat(latitude);
 		var map = new google.maps.Map(document.getElementById('map'), {
+			mapTypeControlOptions: {
+			mapTypeIds: ['mystyle', google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+			},
 			center: {lat: lg, lng: lt},//new google.maps.LatLng(latitude, longitude),
-			zoom: 20
+			zoom: 12,
+			mapTypeId: 'mystyle'
 			});
+		map.mapTypes.set('mystyle', new google.maps.StyledMapType(myStyle, { name: 'My Style' }));
 		// var marker = new google.maps.Marker({
 		// 	position: {lat: lg, lng: lt},
 		// 	map: map
@@ -36,7 +50,11 @@ function initMap() {
 						var infoWindow = new google.maps.InfoWindow();
 						infoWindow.setContent(campus_data[i]['name']);
 						infoWindow.open(map, marker);
-						}	
+						$('.biz-title').remove();
+						$('.biz-photo').remove();
+						$("#biz").prepend("<img src=" + campus_data[i]["photo_url"] + " style='width:30%;height:15%' class='biz-photo'>");
+						$("#business").prepend("<h3 class='biz-title'>" + campus_data[i]["name"] + "</h3>");
+						}
 					})(marker, i));
 				}
 			map.fitBounds(bounds);
