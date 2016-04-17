2ee602c188318a0d6d9922d1b07f0e4ba7894f70
Johnny Yeo <johnnyyeo12@gmail.com>
2016-04-16 20:25:26 -0700

    bug fixes

--- scripts/histogram_business.js
+++ scripts/histogram_business.js
@@ -1,7 +1,7 @@
 //Beginning of d3 code, set up SVG
-var businessmargin = {top: 20, right: 10, bottom: 50, left: 150},
-    businesswidth = 750 - businessmargin.left - businessmargin.right,
-    businessheight = 325 - businessmargin.top - businessmargin.bottom;
+var businessmargin = {top: 25, right: 10, bottom: 50, left: 150},
+    businesswidth = 758 - businessmargin.left - businessmargin.right,
+    businessheight = 300 - businessmargin.top - businessmargin.bottom;
     businesspadding = -150;
 
 var businessx = d3.scale.ordinal()
@@ -16,13 +16,18 @@ var businessxAxis = d3.svg.axis()
     .orient("bottom")
     .tickPadding(5);
 
+var businessyAxis = d3.svg.axis()
+    .scale(businessy)
+    .orient("left")
+    .ticks(10); 
 
-var business_histogram_svg = d3.select(".histogram-business").append("svg")
+var svg = d3.select(".histogram-business").append("svg")
     .attr("width", businesswidth + businessmargin.left + businessmargin.right)
     .attr("height", businessheight + businessmargin.top + businessmargin.bottom)
     .append("g")
     .attr("transform", "translate(" + businessmargin.left + "," + businessmargin.top + ")");    
 
+$('.histogram-business svg').attr("style", "padding:15px 0 0 150px;")
 businessupdate('DO1Ukiuia9hs33VTnTY_Jg')  //Initial output when user first launch the site
 
 //Take in school id and update the data then histogram based on the id
@@ -35,10 +40,7 @@ d3.tsv("../209_FinalProject_Data/histogram_business.tsv", function(error, data) 
    
    c = d3.max(data, function(d) { return d[b]; } )
 
-   var businessyAxis = d3.svg.axis()
-    .scale(businessy)
-    .orient("left")
-    .ticks(c); 
+   svg = d3.select(".histogram-business").selectAll("svg");
   
    //This code needs to be line up with the naming above to work properly
    //SRC="/Users/Maximus/Desktop/W209_DB/Final Project/images.png"></a>
@@ -52,7 +54,7 @@ d3.tsv("../209_FinalProject_Data/histogram_business.tsv", function(error, data) 
   
     var trans = d3.select(".histogram-business").transition().duration(200);
         delay = function(d, i) { return i * 5; };
-      var rect = business_histogram_svg.selectAll(".business-bar")
+      var rect = svg.selectAll(".business-bar")
                     .data(data);
 
            //re-append chart with new data         
@@ -68,23 +70,22 @@ d3.tsv("../209_FinalProject_Data/histogram_business.tsv", function(error, data) 
              .duration(200)
              .attr("height", function(d) { return businessheight - businessy(d[b]); });
 
- business_histogram_svg.select(".business-x axis").remove(); //Remove previous x axis
+ svg.select(".business-xaxis").remove(); //Remove previous x axis
 
- business_histogram_svg.append("g")
-    .attr("class", "business-x axis")
+ svg.append("g")
+    .attr("class", "business-xaxis")
     .attr("transform", "translate(0," + (businessheight)  + ")")
     .call(businessxAxis);
     
-  business_histogram_svg.append("text")
+  svg.append("text")
     .attr("transform", "translate("+ (businesswidth/2) +","+(businessheight + 37)+")")  // centre below axis
     .style("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
     .text("Stars");    
 
+ svg.select(".business-yaxis").remove(); //Remove previous y-axis
 
- business_histogram_svg.select(".business-y axis").remove(); //Remove previous y-axis
-
- business_histogram_svg.append("g")
-    .attr("class", "business-y axis")
+ svg.append("g")
+    .attr("class", "business-yaxis")
     .call(businessyAxis)  
     .append("text")
     .attr("transform", "translate("+ (businesspadding/2) +","+(businessheight/2)+")rotate(-90)")
